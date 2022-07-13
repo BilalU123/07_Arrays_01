0888aa21841c2bdf8b563525a973f8ba25d43cbd
BilalU123 <uddinbilal@aol.com>
2022-07-13 11:18:47 +0200

    getSentenceArr/ Transformation

--- script_01.js
+++ script_01.js
@@ -19,7 +19,6 @@
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
  //output(getSentence("Ich","bin","Max","Mütze"));
-
 //output(getSentence("Ich","bin","Maxine","Mützerich"));
 //output(getSentence("Ich","bin","der","coole", "Max","Mütze"));
 //more of the same!
@@ -36,6 +35,21 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
                     punct;
     return str;
 }
+
+/******** 01a. Funktionalität mit Array ! */
+output(getSentenceArr(["Ich","bin","Max","Mütze"]));
+function getSentenceArr(arr) {
+
+    const gap   = " ";
+    const punct = ".";
+    let str   =     arr[0] + gap +
+                    arr[1] + gap +
+                    arr[2] + gap +
+                    arr[3] +
+                    punct;
+    return str;
+}
+
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
